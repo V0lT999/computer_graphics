aa5426ee5c187994c1ca0da290e56343076c00c1
Vladimir Toropov <toropov29@rambler.ru>
2020-04-28 16:36:38 +0300

    done lab 5
    Theme: "Research of algorithms for detecting the visibility of complex scenes"
    
    Task: ". Обеспечить реализацию алгоритма выявления видимых граней и ребер для одиночного выпуклого объемного тела."

---
+++ lab5/lab1_library.py
@@ -0,0 +1,67 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+import matplotlib.pyplot as plt
+from tkinter import *
+from tkinter.ttk import *
+
+
+def plt_show(data):
+    verts = data[0]
+    res_matrix = data[1]
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    r = [-1, 1]
+
+    X, Y = np.meshgrid(r, r)
+    # plot vertices
+    ax.scatter3D(res_matrix[:, 0], res_matrix[:, 1], res_matrix[:, 2])
+
+    # plot sides
+    ax.add_collection3d(Poly3DCollection(verts, facecolors='cyan', linewidths=1, edgecolors='r', alpha=.25))
+    ax.scatter([1], [-1], [1])
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt. show()
+
+def rotate_figure(os='y', angle_s='0'):
+    angle = int(angle_s)
+
+    matrix = [
+        [0.5, -0.5, -0.5], [0.5,  0.5, -0.5], [-0.5,  0.5, -0.5], [-0.5, -0.5, -0.5],
+        [0.5, -0.5, 0.5], [0.5,  0.5, 0.5], [-0.5,  0.5, 0.5], [-0.5, -0.5, 0.5],
+        [0.5, -0.5, -0.5], [0.5,  0.5, -0.5], [0.5,  0.5,  0.5], [0.5, -0.5,  0.5],
+        [-0.5, -0.5,  0.5], [-0.5,  0.5,  0.5], [-0.5,  0.5, -0.5], [-0.5, -0.5, -0.5],
+        [0.5,  0.5,  0.5], [0.5,  0.5, -0.5], [-0.5,  0.5, -0.5], [-0.5,  0.5,  0.5],
+        [0.5, -0.5, -0.5], [0.5, -0.5,  0.5], [-0.5, -0.5,  0.5], [-0.5, -0.5, -0.5]
+    ]
+    T = [[]]
+    res_matrix = np.zeros((8, 3))
+    cos_count = np.cos(angle)
+    sin_count = np.sin(angle)
+
+    if os == "x":
+        T = np.array([[1, 0, 0], [0, cos_count, sin_count], [0, -sin_count, cos_count]])
+        res_matrix = np.dot(matrix, T.transpose())
+    elif os == "y":
+        T = np.array([[cos_count, 0, -sin_count], [0, 1, 0], [sin_count, 0, cos_count]])
+        res_matrix = np.dot(matrix, T)
+    elif os == "z":
+        T = np.array([[cos_count, sin_count, 0], [-sin_count, cos_count, 0], [0, 0, 1]])
+        res_matrix = np.dot(matrix, T)
+    else:
+        res_matrix = matrix
+
+    verts = [[res_matrix[0], res_matrix[1], res_matrix[2], res_matrix[3]],
+             [res_matrix[4], res_matrix[5], res_matrix[6], res_matrix[7]],
+             [res_matrix[0], res_matrix[1], res_matrix[5], res_matrix[4]],
+             [res_matrix[2], res_matrix[3], res_matrix[7], res_matrix[6]],
+             [res_matrix[1], res_matrix[2], res_matrix[6], res_matrix[5]],
+             [res_matrix[4], res_matrix[7], res_matrix[3], res_matrix[0]],
+             [res_matrix[2], res_matrix[3], res_matrix[7], res_matrix[6]]
+    ]
+
+    return [verts, res_matrix]
---
+++ lab5/lab5.py
@@ -0,0 +1,98 @@
+import sys, os.path
+sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/lab1mpl/'))
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+import matplotlib.pyplot as plt
+from tkinter import *
+from tkinter.ttk import *
+import lab1_library as lb
+
+
+def main():
+    main_window()
+
+
+def Roberts(data):
+    verts = data[0]
+    W = np.zeros(3)
+    P = [1, -1, 1]
+
+    for i in range(2):
+        max_value = -10
+        min_value = 10
+        for point in verts:
+            max_value1 = max(point[j][0] for j in range(len(point)))
+            max_value = max(max_value, max_value1)
+            min_value1 = min(point[j][0] for j in range(len(point)))
+            min_value = min(min_value, min_value1)
+        W[i] = (max_value + min_value)/2
+
+    new_data = []
+    for vector in verts:
+        #Search vectors
+        Vec1_x = vector[0][0] - vector[1][0]
+        Vec2_x = vector[2][0] - vector[1][0]
+        Vec1_y = vector[0][1] - vector[1][1]
+        Vec2_y = vector[2][1] - vector[1][1]
+        Vec1_z = vector[0][2] - vector[1][2]
+        Vec2_z = vector[2][2] - vector[1][2]
+
+        #Search coefficients
+        A = Vec1_y*Vec2_z - Vec2_y*Vec1_z
+        B = Vec1_z*Vec2_x - Vec2_z*Vec1_x
+        C = Vec1_x*Vec2_y - Vec2_x*Vec1_y
+        D = -(A*vector[0][0] + B*vector[0][1] + C*vector[0][2])
+
+        m = -(A*W[0] + B*W[1] + C*W[2] + D)
+
+        A = A*m
+        B = B*m
+        C = C*m
+        D = D*m
+
+        if (A*P[0] + B*P[1] + C*P[2] + D) > 0:
+            new_data.append(vector)
+
+    return new_data
+
+
+def main_window():
+    def click():
+        angle_f = angle_field.get()
+        if angle_f == '':
+            angle_f = '0'
+        data = lb.rotate_figure(os_combo.get(), angle_f)
+        data[0] = Roberts(data)
+        lb.plt_show(data)
+
+    def reset():
+        lb.plt_show(lb.rotate_figure())
+
+
+    window = Tk()
+    window.title("first lab")
+    window.geometry('500x250')
+
+    os_lbl = Label(window, text="Select the axis: ", font=("Arial", 14)).place(x=10, y=10)
+    angle_lbl = Label(window, text="Enter the angle:", font=("Arial", 14)).place(x=10, y=50)
+
+    os_combo = Combobox(window)
+    os_combo["values"] = ("x", "y", "z")
+    os_combo.current(0)
+    os_combo.place(x=150, y=10)
+
+    angle_field = Entry(window, width=10)
+    angle_field.place(x=150, y=50)
+
+    ok_button = Button(window, text="Ok", command=click)
+    ok_button.place(x=150, y=100)
+
+    reset_button = Button(window, text="Reset", command=reset)
+    reset_button.place(x=300, y=100)
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
